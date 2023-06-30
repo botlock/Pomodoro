05f7cf321c66ee0ec0b3907ea4be721bdba0f5c2
BOTLOCK <gustavo.olier27@gmail.com>
2023-06-30 00:06:29 -0300

    First-commit

---
+++ src/Router.tsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from 'react-router-dom'
+import { Home } from './pages/home'
+import { History } from './pages/history'
+import DefaultLayout from './layouts/DefaultLayout'
+
+const Router = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
+      </Route>
+    </Routes>
+  )
+}
+
+export default Router
