f0ca50189944fadd6e5abe42d07e2b55aea4de69
BOTLOCK <gustavo.olier27@gmail.com>
2023-07-03 11:33:58 -0300

    Second-first

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Play } from 'phosphor-react'
+import {
+  CountdownContainer,
+  HomeContainer,
+  FormContainer,
+  Separator,
+  StartCountDownButton,
+} from './styles'
+
+export const Home = () => {
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <input id="task" />
+
+          <label htmlFor="minutesAmount">Durante</label>
+          <input type="number" id="minutesAmount" />
+
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <StartCountDownButton disabled type="submit">
+          <Play size={24} />
+          Começar
+        </StartCountDownButton>
+      </form>
+    </HomeContainer>
+  )
+}
